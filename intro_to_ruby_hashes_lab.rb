bb8a8abcedd12c9d955027bd245f7cf8296a208f
William Pannell <lpann5@gmail.com>
2019-10-18 06:06:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,3 +25,16 @@ def monopoly_with_third_tier
   }
 end
 
+def monopoly_with_fourth_tier
+  naruto = {
+  railroads: {:pieces => 4,
+  :rent_in_dollars => {:one_piece_owned => 25,:two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200},
+  :names => {:reading_railroad => {}, :pennsylvania_railroad => {:mortgage_value => ""}, :b_and_o_railroad => {}, :shortline_railroad => {} }}
+  }
+  monopoly[:railroads][:names][:reading_railroad]["mortgage_value"]="$100"
+  monopoly[:railroads][:names][:pennsylvania_railroad]["mortgage_value"]="$200"
+  monopoly[:railroads][:names][:b_and_o_railroad]["mortgage_value"]="$400"
+  monopoly[:railroads][:names][:shortline_railroad]["mortgage_value"]="$800"
+  monopoly
+  p naruto[:railroads][:names][:reading_railroad]["mortgage_value"]="100"
+end
